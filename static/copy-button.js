3f87d78b33d756a936ec654cf5646599320b20df
David Lapshin <ddaudix@gmail.com>
2024-05-09 22:01:25 +0300

    Add optional copy code button!

---
+++ static/copy-button.js
@@ -0,0 +1,31 @@
+// Based on https://www.roboleary.net/2022/01/13/copy-code-to-clipboard-blog.html
+document.addEventListener("DOMContentLoaded", function () {
+    let blocks = document.querySelectorAll("pre[class^='language-']");
+
+    blocks.forEach((block) => {
+        if (navigator.clipboard) {
+            let button = document.createElement("button");
+            let icon = document.createElement("i");
+
+            button.appendChild(icon);
+            block.appendChild(button);
+
+            button.addEventListener("click", async () => {
+                await copyCode(block, button);
+            });
+        }
+    });
+
+    async function copyCode(block, button) {
+        let code = block.querySelector("code");
+        let text = code.innerText;
+
+        await navigator.clipboard.writeText(text);
+
+        button.classList.add("active");
+
+        setTimeout(() => {
+            button.classList.remove("active");
+        }, 800);
+    }
+});
